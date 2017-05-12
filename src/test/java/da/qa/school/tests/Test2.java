475f2de2f751f6a8bf23e105185ad173bdb2dab0
Konstantin Kovbasa <kkovbasa@dataart.com>
2017-05-12 15:33:51 +0300

    new tests

--- src/test/java/da/qa/school/tests/Test2.java
+++ src/test/java/da/qa/school/tests/Test2.java
@@ -3,7 +3,7 @@ package da.qa.school.tests;
 import static com.codeborne.selenide.Selenide.open;
 import da.qa.school.tests.config.BaseTest;
 import da.qa.shool.pages.SearchPage;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Features;
 
