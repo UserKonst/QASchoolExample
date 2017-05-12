ee735091ccc71d8666edfcf9a5ec6763eaa6ea45
konstjobs <kostyan.vk@gmail.com>
2017-05-12 17:04:25 +0300

    small fix

---
+++ src/test/java/da/qa/school/tests/good/Test2.java
@@ -0,0 +1,23 @@
+package da.qa.school.tests.good;
+
+import static com.codeborne.selenide.Selenide.open;
+import da.qa.school.tests.config.BaseTest;
+import da.qa.shool.pages.SearchPage;
+import da.qa.shool.pages.SearchResultPage;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+
+@Features({"Some other feature 2"})
+public class Test2 extends BaseTest {
+
+    private SearchPage searchPage;
+    private SearchResultPage resultPage;
+    private final String TEXT = "automated-testing.info";
+
+    @Test
+    public void first_search_result_should_contains_automated_testing() {
+        searchPage = open("https://google.com.ua", SearchPage.class);
+        resultPage = searchPage.search(TEXT);
+        String resultText = resultPage.getFirstResultHeader();
+    }
+}
