4bae29987902dbfa5f1c8c10b1899f232bde2928
konstjobs <kostyan.vk@gmail.com>
2017-05-12 16:36:20 +0300

    aspect j version changed

--- src/test/java/da/qa/school/tests/Test4.java
+++ src/test/java/da/qa/school/tests/Test4.java
@@ -5,7 +5,9 @@ import da.qa.school.tests.config.BaseTest;
 import da.qa.shool.pages.BasePage;
 import da.qa.shool.pages.SearchPage;
 import da.qa.shool.pages.SearchResultPage;
+import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.*;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Features;
 
@@ -16,6 +18,11 @@ public class Test4 extends BaseTest {
     private SearchResultPage resultPage;
     private BasePage basePage;
 
+    @BeforeClass
+    public void pause() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
+    }
+    
     @Test
     public void page_url_should_contains_google_com() {
         searchPage = open("https://google.com.ua", SearchPage.class);
